0595c575d84f08426d01cea6771698dd8e7f4b11
Carlos Villatoro <crvb0797@gmail.com>
2021-05-12 13:49:43 -0600

    Creando la vista show para cada uno de los post, filtrando para mostrar post similares en un sidebar

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -12,4 +12,10 @@ class PostController extends Controller
         $posts = Post::where('status', 2)->latest('id')->paginate(8); //Regresaremos unicamente los post con el estatus de publicado.
         return view('posts.index', compact('posts'));
     }
+
+    public function show(Post $post)
+    {
+        $similares = Post::where('category_id', $post->category_id)->where('status', 2)->where('id', '!=', $post->id)->latest('id')->take(4)->get();
+        return view('posts.show', compact('post', 'similares'));
+    }
 }
