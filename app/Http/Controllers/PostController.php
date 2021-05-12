bd539d17e713262b8d9aa223d0e65a5ea69add73
Carlos Villatoro <crvb0797@gmail.com>
2021-05-12 16:20:16 -0600

    creando el filtao por categoria, listo y funcional

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Post;
+use App\Models\Category;
 
 class PostController extends Controller
 {
@@ -17,5 +18,11 @@ class PostController extends Controller
     {
         $similares = Post::where('category_id', $post->category_id)->where('status', 2)->where('id', '!=', $post->id)->latest('id')->take(4)->get();
         return view('posts.show', compact('post', 'similares'));
+    }
+
+    public function category(Category $category)
+    {
+        $posts = Post::where('category_id', $category->id)->where('status', 2)->latest('id')->paginate(6);
+        return view('posts.category', compact('posts', 'category'));
     }
 }
